2208b410ae2b0b914daa74dbe5fbe2c240557ed2
sarvagya-tech <sarvagyam93@gmail.com>
2024-12-12 14:52:04 +0000

    container config done

---
+++ .devcontainer/01_basics/variable.js
@@ -0,0 +1,8 @@
+const accountId = 1234
+let accountEmail = "sarvagya@gmail.com"
+var accountPassword = "7890"
+accountCity = "jaipur"
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity])
+
